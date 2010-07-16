671f676d53b563361ea4ea3f605637fc3a983686
Dimitris Moraitis <dimo@unweb.me>
2010-07-16 14:23:10 +0000

    update setup.py
    svn path=/collective.transcode/trunk/; revision=120992

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@ from setuptools import setup, find_packages
 import os
 
 version = '0.2'
+name = 'collective.transcode'
+path = ['src'] + name + name.split('.') + ['version.txt']
+version = open(join(*path)).read().strip()
+readme = open('README.txt').read()
+history = open(join('docs', 'HISTORY.txt')).read()
+tests_require = ['collective.monkeypatcher']
 
-setup(name='collective.transcode',
+
+setup(name=name,
       version=version,
       description="Transcoding support for Plone video files, using collective.transcode.daemon",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n" +
-                       open("AUTHORS.txt").read() + "\n",
+      long_description = readme[readme.find('\n\n'):] + '\n' + history,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -20,7 +25,8 @@ setup(name='collective.transcode',
       author_email='we@unweb.me',
       url='https://svn.plone.org/svn/collective/collective.transcode',
       license='GPL',
-      packages=find_packages('src/collective.transcode/',exclude=['ez_setup']),
+      package_dir = {'': 'src/collective.transcode'},
+      packages=find_packages('src/collective.transcode/'),
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
