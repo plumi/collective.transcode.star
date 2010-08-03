16deeca6ec5c5f37fa179e2ab4559e5a6888e815
Dimitris Moraitis <dimo@unweb.me>
2010-08-03 23:12:04 +0000

    prepare 0.4 release
    svn path=/collective.transcode.star/trunk/; revision=122751

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 name = 'collective.transcode.star'
-version = '0.3'
+version = '0.4'
 
 long_description = (
     read('README.txt')
