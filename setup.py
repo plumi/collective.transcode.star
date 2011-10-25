245bd668ef75ed137c27c48c80a07b39d8140214
Mike Muzurakis <clopy@unweb.me>
2011-10-25 10:15:15 +0300

    added mediaelementsjs player

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name=name,
           'plone.app.registry',
           'pycrypto',
           'hashlib',
-          'collective.flowplayer',
+          'collective.mediaelementsjs',
       ],
       entry_points="""
       # -*- Entry points: -*-
