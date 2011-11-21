0355c922c17fea43ca5a613bee2225c4dffb6250
Christos Psaltis <cpsaltis@unweb.me>
2011-11-21 19:25:30 +0200

    Get the dict of profiles and serve the low versions first

--- collective/transcode/star/viewlets.py
+++ collective/transcode/star/viewlets.py
@@ -3,24 +3,15 @@ from plone.app.layout.viewlets.common import ViewletBase
 from collective.transcode.star.interfaces import ITranscodeTool
 from zope.component import getUtility
 
+
 class TranscodeViewlet(ViewletBase):
     render = ViewPageTemplateFile('viewlet.pt')
 
     def update(self):
         tt = getUtility(ITranscodeTool)
         uid = self.context.UID()
-        
-        try:    
-            self.mp4 = tt[uid][tt[uid].keys()[0]]['mp4']
-        except KeyError:
-            pass
-        
+
         try:
-            self.webm = tt[uid][tt[uid].keys()[0]]['webm']
-        except KeyError:
-            pass
-        
-        try:
-            self.jpeg = tt[uid][tt[uid].keys()[0]]['jpeg']
+            self.profiles = tt[uid][tt[uid].keys()[0]]
         except KeyError:
             pass
