0dac2ecb681ade02498fa6053946faef428df0be
Dimitris Moraitis <dimo@unweb.me>
2011-12-17 19:01:34 +0200

    update changelog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 name = 'collective.transcode.star'
-version = '0.10'
+version = '0.11'
 
 long_description = (
     read('README.txt')
