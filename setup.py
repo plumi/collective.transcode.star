4e5220087d5deaf43b869c97217d1bec896f7ad4
Dimitris Moraitis <dimo@unweb.me>
2012-12-03 19:07:48 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 name = 'collective.transcode.star'
-version = '0.19'
+version = '0.20'
 
 long_description = (
     read('README.txt')
